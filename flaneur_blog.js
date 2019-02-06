92b51b82c70793eecdcbf7f46ea20872a9630987
Peter I Hansen <44348812+pivey@users.noreply.github.com>
2019-02-06 14:59:35 +0100

    added responsiveness, style changes and updated the header and footer

--- flaneur_blog.js
+++ flaneur_blog.js
@@ -1,7 +1,5 @@
-
-	var prevScrollpos = window.pageYOffset;
-
-	window.onscroll = function() {
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("header").style.top = "0";
@@ -9,5 +7,13 @@
     document.getElementById("header").style.top = "-50px";
   }
   prevScrollpos = currentScrollPos;
-}
+};
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+};
+
+
 
